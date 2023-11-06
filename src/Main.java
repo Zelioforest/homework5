63d9758f7be27e0b8d38d6fea3697dfdf8444de5
Zelioforest <veshky@mail.ru>
2023-11-06 19:32:05 +0300

    homework5 1 attempt

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,86 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // tack 1
+        int clientOS = 5; //  (0 — iOS, 1 — Android)
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Нет ссылки для вашей системы");
+        }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        //tack 2
+        int clientOSNew = 1; //  (0 — iOS, 1 — Android)
+        int clientDeviceYear = 2015;
+        if (clientOSNew == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else if (clientOSNew == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+
+        // tack 3
+        int year = 1800;
+        if (year > 1584) {
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }
+        }
+
+        // tack 4
+        int timeDelivery = 1;
+        int deliveryDistance = 95;
+        if (deliveryDistance < 100) {
+            if (deliveryDistance >= 60) {
+                timeDelivery += 2;
+                System.out.println("Потребуется дней: " + timeDelivery);
+            } else if (20 <= deliveryDistance && deliveryDistance < 60) {
+                timeDelivery += 1;
+                System.out.println("Потребуется дней: " + timeDelivery);
+            } else {
+                System.out.println("Потребуется дней: " + timeDelivery);
+            }
+        } else {
+            System.out.println("Нет доставки");
+        }
+
+        // tack 5
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Этот месяц принадлежит к сезону зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Этот месяц принадлежит к сезону весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Этот месяц принадлежит к сезону лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Этот месяц принадлежит к сезону осень");
+                break;
+            default:
+                System.out.println("Неверно введен номер месяца");
         }
     }
 }
